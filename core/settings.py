d481311dbc350a2b2f558b6573edc3614e56c9d1
Nicat-N <nicatniftaliyev00@gmail.com>
2025-04-03 14:25:42 +0000

    scommit

--- core/settings.py
+++ core/settings.py
@@ -32,9 +32,13 @@ INSTALLED_APPS = [
     #"django.contrib.staticfiles",
     "rest_framework",
     "api",
+    "corsheaders",
+
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -43,6 +47,12 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+
+CORS_ALLOWED_ORIGINS = [
+    "https://github.com/Nicat-N",
+]
+
 
 ROOT_URLCONF = "core.urls"
 
